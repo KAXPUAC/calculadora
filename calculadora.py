6aa0a1a74b01c2752ce8284e7d5f71f8f42acd4b
Kevin Axpuac <kevinjuarez@galileo.edu>
2022-06-01 00:10:00 -0600

    Se agrega codigo

---
+++ calculadora.py
@@ -0,0 +1,134 @@
+#Imprime el error generado, o error general
+def error(str = ''):
+    if len(str):
+        print("resultado >> ERROR! Expresión no valida, ", str)
+    else:
+        print("resultado >> ERROR! Expresión no valida")
+
+#Devuelve 1 si la palabra contiene un caracter
+def contiene(str, letra):
+    existe = 0
+    for caracter in str:
+        if caracter == letra:
+            existe = 1
+            break
+    return existe
+
+# Cuenta la cantidad de coincidencias en el caracter que se busca
+def contar(str, letra):
+    aux = 0
+    for caracter in str:
+        if caracter == letra:
+            aux = aux + 1
+    return aux
+
+#Valida si lo recibido es un Numero
+def esNumero(str):
+    try:
+        valFloat = float(str)
+        valInt = int(str)
+        if (valFloat - valInt) == 0:
+            return valInt
+        return valFloat
+    except ValueError:
+        return 'False'
+
+#Cuenta los espacios que hay entre cada caracter
+def espacios(str):
+    espacios = 0
+    anterior = ''
+    espacio = ' '    
+    for x in range(len(str)):
+        if x == 0:
+            anterior = str[x]
+            continue
+        if anterior == espacio and str[x] == espacio:
+            espacios = espacios + 1
+        anterior = str[x]
+    return (espacios == 0)
+
+
+#Valida que en la expresion no exita un espacios entre parentesis,  numero u operador
+def expresionCorrecta(str):
+    return (str[1] != ' ' and str[len(str) - 2]  != ' ')
+
+def posLastChar(str, letra):
+    position = 0
+    for x in range(len(str)):
+        if str[x] == letra:
+            position = x
+    return position
+
+def posFirstChar(str, letra):
+    position = 0
+    for x in reversed(range(len(str))):
+        if str[x] == letra:
+            position = x
+    return position
+
+def realizarOperacion(commands):
+    resultado = 0
+    if commands[0] == '-':
+        if esNumero(commands[1]) and esNumero(commands[2]):
+            resultado = esNumero(commands[1]) - esNumero(commands[2])
+    elif commands[0] == '+':
+        if esNumero(commands[1]) and esNumero(commands[2]):
+            resultado = esNumero(commands[1]) + esNumero(commands[2])
+    elif commands[0] == '*':
+        if esNumero(commands[1]) and esNumero(commands[2]):
+            resultado = esNumero(commands[1]) * esNumero(commands[2])
+    elif commands[0] == '/':
+        if esNumero(commands[1]) and esNumero(commands[2]):
+            resultado = esNumero(commands[1]) / esNumero(commands[2])
+    return resultado
+
+def analizarCommando(entrada):
+    if esNumero(entrada) != 'False':
+        print('respuesta >> ', entrada)
+    else:
+        command = entrada[posLastChar(entrada, '('): len(entrada)]
+        command = command[0:posFirstChar(command, ')') + 1]
+        if expresionCorrecta(command):
+            strAux = entrada[0:posLastChar(entrada, '(')]
+            strAux2 = entrada[posLastChar(entrada, '(') + len(command): len(entrada)]
+            resultado = 0
+            command = command[1: len(command) - 1]
+            commands = command.split(' ')
+            if len(commands) == 3:
+                resultado = realizarOperacion(commands)            
+            else:
+                error('Faltan argumentos')
+            entrada = strAux + str(resultado) + strAux2
+            analizarCommando(entrada)
+
+
+#Interfaz con el usuario
+def init():
+    loop = 1
+    while loop:
+        command = input("calculaodra >> ").strip()
+        if command == "":
+            error()
+            continue
+        elif command == 'quit':
+            print("Saliendo...")
+            print("Gracias por usar calculadora.")
+            break
+        elif contiene(command, '('):
+            if contar(command, '(') == contar(command, ')'):
+                if espacios(command):
+
+                   analizarCommando(command)
+
+                else:
+                    error('existen espacios de más, o faltan espacios')
+            else:
+                error('Faltan parentesis')
+            continue
+        elif esNumero(command) != 'False':
+            print(esNumero(command))
+            continue
+        else:
+            error()
+init()
+
